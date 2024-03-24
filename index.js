027b6137c4d9618634957c25c607e9c2413c1335
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-24 20:37:14 +0600

    removed comments and spaces

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eted0lc.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -36,10 +35,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
 
-    // users, admin and moderator related
     const allGamesCollection = client.db('gamesCollection').collection('games');
     const timerGameCollection = client.db('gamesCollection').collection('timerGame');
 
@@ -49,16 +45,11 @@ async function run() {
     });
 
 
-   
-
-    // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
